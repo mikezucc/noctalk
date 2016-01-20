2a2c2bc8a5a8c2afbcef8ad40a7d3b157c6a34dc
Michael Zuccarino <mikezuccarino@gmail.com>
2016-01-19 19:58:37 -0800

    iOS set up

---
+++ ios/NocTalk/NocTalk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NocTalk
+//
+//  Created by Michael Zuccarino on 1/19/16.
+//  Copyright © 2016 MemeTownUSA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
